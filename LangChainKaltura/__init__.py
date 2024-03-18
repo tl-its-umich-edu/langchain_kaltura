34fcc8d5b4618fabe781d1b8d19770272c37b7bf
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-03-18 12:01:17 -0400

    #8 - test suite support and Kaltura mocking

---
+++ LangChainKaltura/__init__.py
@@ -0,0 +1,1 @@
+from .kaltura_caption_loader import KalturaCaptionLoader
---
+++ LangChainKaltura/tests/tests.py
@@ -0,0 +1,44 @@
+import json
+import os.path
+
+import flask
+from http_server_mock import HttpServerMock
+
+from LangChainKaltura import KalturaCaptionLoader
+
+HOST_DEFAULT = 'localhost'
+PORT_DEFAULT = 8311
+
+app = HttpServerMock(__name__)
+
+fixturesPathname = os.path.join(os.path.dirname(__file__), 'fixtures', '')
+
+
+@app.route('/api_v3/service/<service>/action/<action>',
+           methods=['POST'])
+def serviceActionHandler(service, action):
+    (host, port) = flask.request.server
+    return open(f'{fixturesPathname}{service}_{action}.xml').read().format(
+        host=host, port=port)
+
+
+# contrived route, specified in `caption_captionasset_getUrl.xml`
+@app.route('/captionAsset/contents',
+           methods=['GET'])
+def captionAssetContents():
+    return open(f'{fixturesPathname}pinball_wizard.srt').read()
+
+
+def main(host: str = HOST_DEFAULT, port: int = PORT_DEFAULT):
+    with app.run(host, port):
+        kalturaCaptions = KalturaCaptionLoader(
+            '_partner_ID_here_',
+            '_app_token_ID_here_',
+            '_app_token_value_here_',
+            KalturaCaptionLoader.FilterType.MEDIAID,
+            '1_mediaId',
+            'https://example.edu/v/{mediaId}?t={startSeconds}',
+            kalturaApiBaseUrl=f'http://{host}:{port}/')
+        documents = kalturaCaptions.load()
+        print(json.dumps([d.to_json()['kwargs'] for d in documents],
+                         indent=2, sort_keys=True))
