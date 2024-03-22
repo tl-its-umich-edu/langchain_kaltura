67626819d2cc49d66b56cf18e6435330a0c65223
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-03-22 10:39:31 -0400

    #6 - comment out dist. steps until needed

--- distribute.sh
+++ distribute.sh
@@ -2,6 +2,6 @@
 
 python3 -m pip install --upgrade build
 python3 -m build
-python3 -m pip install --upgrade twine
-python3 -m twine upload --repository testpypi dist/*
-#python3 -m twine upload dist/*
+#python3 -m pip install --upgrade twine
+#python3 -m twine upload --repository testpypi --skip-existing dist/*
+#python3 -m twine upload --skip-existing dist/*
