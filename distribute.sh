6a50849a7e1a03732f51585d35394345527b6c35
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-03-25 15:49:11 -0400

    #6 - removing semi-specific Python version number
    It will use whichever Python is installed in .venv.

--- distribute.sh
+++ distribute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh --
 
-python3 -m pip install --upgrade build
-python3 -m build
-#python3 -m pip install --upgrade twine
-#python3 -m twine upload --repository testpypi --skip-existing dist/*
-#python3 -m twine upload --skip-existing dist/*
+python -m pip install --upgrade build
+python -m build
+#python -m pip install --upgrade twine
+#python -m twine upload --repository testpypi --skip-existing dist/*
+#python -m twine upload --skip-existing dist/*
