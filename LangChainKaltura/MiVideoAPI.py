40bb96c412ea641e77ab6644b77c15f45317cdc3
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-10-01 16:10:00 -0400

    #13 - version argument and timeout with retry
    Due to intermittently long response times from the API, set a low number of seconds for each request to timeout, then retry the request with exponentially increasing wait times between attempts.

--- LangChainKaltura/MiVideoAPI.py
+++ LangChainKaltura/MiVideoAPI.py
@@ -1,10 +1,15 @@
 import base64
+import logging
 import os
 
 import requests
 from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_exponential, before_log
 
 from .AbstractMediaPlatformAPI import AbstractMediaPlatformAPI
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class MiVideoAPI(AbstractMediaPlatformAPI):
@@ -22,47 +27,72 @@ class MiVideoAPI(AbstractMediaPlatformAPI):
     opposed to Kaltura's proprietary method.
     """
 
-    def __init__(self, host, authId, authSecret):
+    _METHOD_GET = 'GET'
+    _METHOD_POST = 'POST'
+
+    def __init__(self, host, authId, authSecret, version='v1', timeout=2):
         self.host = host
-        self.baseUrl = f'https://{self.host}/um/aa/mivideo/v1'
+        self.baseUrl = f'https://{self.host}/um/aa/mivideo/{version}'
+        self.timeout = timeout
         self.headers = {
             'Authorization': self._getAuthToken(authId, authSecret)}
+
+    @retry(stop=stop_after_attempt(3), wait=wait_exponential(max=10),
+           before=before_log(logger, logging.INFO))
+    def _requestWithRetry(self, url, method=_METHOD_GET, params=None,
+                          headers=None):
+        """
+        Necessary to avoid intermittent long delays in response times.
+
+        :param url: URL to request
+        :param method: HTTP method to use (Default: 'GET')
+        :param params: URL parameters (Default: None)
+        :param headers: HTTP headers (Default: None)
+        """
+        response = requests.request(
+            method, url, params=params, headers=headers, timeout=self.timeout)
+        response.raise_for_status()
+        return response
 
     def _getAuthToken(self, authId, authSecret):
         auth = f'{authId}:{authSecret}'
         authBase64 = base64.b64encode(auth.encode('utf-8')).decode('utf-8')
+
         url = f'https://{self.host}/um/oauth2/token'
         params = {'grant_type': 'client_credentials', 'scope': 'mivideo'}
         headers = {'Authorization': f'Basic {authBase64}'}
-        response = requests.post(url, params=params, headers=headers)
+
+        response = self._requestWithRetry(
+            url, method=self._METHOD_POST, params=params, headers=headers)
         response.raise_for_status()
         tokenData = response.json()
-        print('_getAuthToken', response.elapsed.total_seconds())
+        logger.info(f'_getAuthToken {response.elapsed.total_seconds()}s')
         return f"{tokenData['token_type']} {tokenData['access_token']}"
 
     def getMediaList(self, courseId, userId, pageIndex=1, pageSize=500):
         url = f'{self.baseUrl}/course/{courseId}/media'
         params = {'pageIndex': pageIndex, 'pageSize': pageSize}
         headers = {'LMS-User-Id': userId, **self.headers}
-        response = requests.get(url, params=params, headers=headers)
+        response = self._requestWithRetry(url, params=params,
+                                          headers=headers)
         response.raise_for_status()
-        print('getMediaList', courseId, response.elapsed.total_seconds())
+        logger.info(f'getMediaList {response.elapsed.total_seconds()}s')
         return response.json().get('objects', [])
 
     def getCaptionList(self, courseId, userId, mediaId):
         url = f'{self.baseUrl}/course/{courseId}/media/{mediaId}/captions'
         headers = {'LMS-User-Id': userId, **self.headers}
-        response = requests.get(url, headers=headers)
+        response = self._requestWithRetry(url, headers=headers)
         response.raise_for_status()
-        print('getCaptionList', mediaId, response.elapsed.total_seconds())
+        logger.info(f'getCaptionList {response.elapsed.total_seconds()}s')
         return response.json().get('objects', [])
 
     def getCaptionText(self, courseId, userId, captionId):
         url = f'{self.baseUrl}/course/{courseId}/captions/{captionId}/text'
         headers = {'LMS-User-Id': userId, **self.headers}
-        response = requests.get(url, headers=headers)
+        response = self._requestWithRetry(url, headers=headers)
         response.raise_for_status()
-        print('getCaptionText', captionId, response.elapsed.total_seconds())
+        logger.info(f'getCaptionText {response.elapsed.total_seconds()}s')
         return response.text
 
 
