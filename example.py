048ef7c723856abc58790cfa810c267720e85225
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2024-11-19 13:15:52 -0500

    #13 - log level, clean-up, lang. supp., summary

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from typing import List
 
@@ -8,14 +9,12 @@ from langchain_core.documents import Document
 from LangChainKaltura import KalturaCaptionLoader
 from LangChainKaltura.MiVideoAPI import MiVideoAPI
 
+logging.basicConfig(level=logging.INFO)
+
 
 def main() -> List[Document]:
     # Load environment variables from `.env` file
     load_dotenv()
-
-    # JSON not required, but useful for debugging
-    mediaFilter = json.loads(
-        os.getenv('FILTERJSON', '{}'))
 
     api = MiVideoAPI(
         host=os.getenv('MIVIDEO_API_HOST'),
@@ -23,10 +22,17 @@ def main() -> List[Document]:
         authSecret=os.getenv('MIVIDEO_API_AUTH_SECRET'),
     )
 
+    languages = os.getenv('LANGUAGE_CODES_CSV')
+    if not languages:
+        languages = KalturaCaptionLoader.LANGUAGES_DEFAULT
+    else:
+        languages = set(languages.split(','))
+
     captionLoader = KalturaCaptionLoader(
         apiClient=api,
         courseId=os.getenv('COURSEID'),
         userId=os.getenv('USERID'),
+        languages=languages,
         urlTemplate=os.getenv('URLTEMPLATE'),
         chunkSeconds=int(os.getenv('CHUNKSECONDS')
                          or KalturaCaptionLoader.CHUNK_SECONDS_DEFAULT),
@@ -41,3 +47,4 @@ if '__main__' == __name__:
     documents = main()
     print(json.dumps([d.to_json()['kwargs'] for d in documents],
                      indent=2, sort_keys=True))
+    print('Number of Documents:', len(documents))
